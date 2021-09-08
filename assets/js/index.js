56f246a3e7a6d0b016b98c85f47edfd7fc796936
patoGarci <garcia.pato1985@gmail.com>
2021-09-08 20:59:44 +0200

    arreglando el js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,4 @@
 const cards = document.querySelector("#cards");
-cards.addEventListener("click",(e)=>{
-    let selectedCard = e.target.parentNode.parentNode;
-    let cardAdd = document.querySelectorAll(".card-items-active")[selectedCard.getAttribute('data-id')];
-    let borderBoxPink = cardAdd.querySelector(".border-box-pink");
-    let buttonCardSelected = cardAdd.querySelector("button");
-    //coloca el backgroun del boton rosado cuando se activa
-    buttonCardSelected.style.background = " #FF0080";
-    //Activa el borde rosa, externo
-    borderBoxPink.style.opacity="1"
-})
-
-
-//primera vez en la pagina
-onload()
 function onload(){
     if (localStorage.onload) {
         localStorage.onload = Number(localStorage.onload) + 1;
@@ -21,3 +7,17 @@ function onload(){
         modal.style.display ="block";
     }
 }
+//Agrega un recuadro pequeño del lado izquierdo de la tarjeta de cada servicio
+cards.addEventListener("click",(e)=>{
+    let selectedCard = e.target.parentNode.parentNode;
+    let cardAdd = document.querySelectorAll(".card-items-active")[selectedCard.getAttribute('data-id')];
+    let borderBoxPink = cardAdd.querySelector(".border-box-pink");
+    let buttonCardSelected = cardAdd.querySelector("button");
+    //coloca el backgroun del boton rosado cuando se activa
+    buttonCardSelected.style.background = " #ºFF0080";
+    //Activa el borde rosa, externo
+    borderBoxPink.style.opacity="1"
+})
+
+// cuando carga la pagina por primera vez debe salir un mensaje en el modal y sino cuenta la cantidad de visitas a la pagina
+onload()
